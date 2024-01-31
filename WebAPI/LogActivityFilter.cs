736cd0f5b23bf0a854c97f13a6498bf36f95b127
Hesham Fathy <heshamfathykamal@gmail.com>
2024-01-31 05:38:06 +0200

    add LogActivityFilter file and update program.cs

---
+++ WebAPI/LogActivityFilter.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.EntityFrameworkCore.SqlServer.Query.Internal;
+
+namespace WebAPI
+{
+    public class LogActivityFilter : IActionFilter
+
+    {
+        private ILogger<LogActivityFilter> _logger;
+
+        public LogActivityFilter(ILogger<LogActivityFilter> logger)
+        {
+            _logger = logger;
+        }
+
+        // first action 
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            _logger.LogInformation($"Executing action {context.ActionDescriptor.DisplayName} on controller {context.Controller}");
+        }
+
+        // third action after original action (getProduct OR deleteproduct etc..)
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            _logger.LogInformation($"Action {context.ActionDescriptor.DisplayName} is Excecuted on controller {context.Controller}");
+        }
+
+    }
+}
