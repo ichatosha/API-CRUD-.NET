ed98e35652ace07a77767e8efbfefc4d84b46e3f
Hesham Fathy <heshamfathykamal@gmail.com>
2024-01-31 05:40:23 +0200

    update program.cs

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -10,7 +10,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options => {
+    
+    //dependency inversion
+    options.Filters.Add<LogActivityFilter>();
+
+    });
+
+
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
